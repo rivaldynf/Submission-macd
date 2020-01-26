4c3bbd3ea795318b4dcd041923465b6bf02d0777
Rivaldy Nur Fachriza <47944740+rivaldynf@users.noreply.github.com>
2020-01-26 22:04:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -73,7 +73,8 @@
     <h1>Analyze image:</h1>
     Enter the URL to an image, then click the <strong>Analyze image</strong> button.
     <br><br>
-    <input type="file" name="inputImage" id="inputImage" accept=".jpeg,.jpg,.png"/>
+    Image to analyze:
+    <input type="file" name="inputImage" id="inputImage">
     <button onclick="processImage()">Analyze image</button>
     <br><br>
     <div id="wrapper" style="width:1020px; display:table;">
